7fd6608a4519ee68ee3aa7345bf7361804d0e618
kristina-lim <kristinahlim@gmail.com>
2022-12-29 15:58:05 -0800

    Fix header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,35 +7,44 @@ export default function Header() {
       <nav className="nav container">
         <a href="index.html" className="nav_logo">Lim</a>
         <div className="nav_menu">
-          <ul className="nav_list grid">
+          <ul className="nav_list">
             <li className="nav_item">
               <a href="#home" className="nav_link">
-                <i className="uil uil-estate nav_icon"></i> Home
+                <i className="uil uil-estate nav_icon"></i> 
+                Home
               </a>
             </li>
             <li className="nav_item">
               <a href="#about" className="nav_link">
-                <i className="uil uil-user nav_icon"></i> About
+                <i className="uil uil-user nav_icon"></i> 
+                About
               </a>
             </li>
             <li className="nav_item">
               <a href="#skills" className="nav_link">
-                <i className="uil uil-file-alt nav_icon"></i> Skills
+                <i className="uil uil-file-alt nav_icon"></i> 
+                Skills
               </a>
             </li>
             <li className="nav_item">
               <a href="#projects" className="nav_link">
-                <i className="uil uil-briefcase-alt nav_icon"></i> Projects
+                <i className="uil uil-briefcase-alt nav_icon"></i> 
+                Projects
               </a>
             </li>
             <li className="nav_item">
               <a href="#contact" className="nav_link">
-                <i className="uil uil-message nav_icon"></i> Contact
+                <i className="uil uil-message nav_icon"></i> 
+                Contact
               </a>
             </li>
           </ul>
+          <i class="uil uil-times nav_close"></i>
+        </div>
+        <div className="nav_toggle">
+          <i className="uil uil-apps"></i>
         </div>
       </nav>
     </header>
-  )
+  );
 }
