fc07291c8b2067f9ca25300c78f58dd5aeaf54b5
kristina-lim <kristinahlim@gmail.com>
2022-12-28 18:09:23 -0800

    Add NavBar, css, and skills

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,26 @@
+import React, { useRef } from 'react';
 import './NavBar.css';
 
 export default function NavBar() {
+  const section1Ref = useRef(null);
+  const section2Ref = useRef(null);
+  const section3Ref = useRef(null);
+  const section4Ref = useRef(null);
 
+  const scrollToSection = sectionRef => {
+    sectionRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
 
   return (
     <>
+      <nav>
+        <ul>
+          <li><a href="#" onClick={() => scrollToSection(section1Ref)}>Technical Skills</a></li>
+          <li><a href="#" onClick={() => scrollToSection(section2Ref)}>Projects</a></li>
+          <li><a href="#" onClick={() => scrollToSection(section3Ref)}>Resume</a></li>
+          <li><a href="#" onClick={() => scrollToSection(section4Ref)}>Contact Me</a></li>
+        </ul>
+      </nav>
     </>
-  )
+  );
 }
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,11 +1,81 @@
 import NavBar from '../../components/NavBar/NavBar';
 import './App.css';
 
-export default function App() {
+export default function App({ section1Ref, section2Ref, section3Ref, section4Ref }) {
   return (
     <div className="App">
       <NavBar />
-      
+      <div ref={section1Ref}>
+        <h2>Technical Skills</h2>
+        <label>
+          <img src="https://cdn-icons-png.flaticon.com/512/3097/3097978.png" width="70" height="70" />
+          <br></br>
+          JavaScript
+        </label>
+        <label>
+          <img src="https://cdn-icons-png.flaticon.com/512/3104/3104037.png" width="70" height="70" />
+          <br></br>
+          HTML
+        </label>
+        <label>
+          <img src="https://cdn-icons-png.flaticon.com/512/3097/3097811.png" width="70" height="70" />
+          <br></br>
+          CSS
+        </label>
+        <label>
+          <img src="https://cdn-icons-png.flaticon.com/512/180/180867.png" width="70" height="70" />
+          <br></br>
+          Python
+        </label>
+        <label>
+          <img src="https://cdn-icons-png.flaticon.com/512/861/861054.png" width="70" height="70" />
+          <br></br>
+          Mongo
+        </label>
+        <label>
+          <img src="https://cdn-icons-png.flaticon.com/512/477/477532.png" width="70" height="70" />
+          <br></br>
+          Express.js
+        </label>
+        <label>
+          <img src="https://cdn-icons-png.flaticon.com/512/4069/4069174.png" width="70" height="70" />
+          <br></br>
+          React.js
+        </label>
+        <label>
+          <img src="https://cdn-icons-png.flaticon.com/512/919/919825.png" width="70" height="70" />
+          <br></br>
+          Node.js
+        </label>
+        <label>
+          <img src="https://i.imgur.com/r72rvmz.png" width="70" height="70" />
+          <br></br>
+          Django
+        </label>
+        <label>
+          <img src="https://cdn-icons-png.flaticon.com/512/5968/5968388.png" width="70" height="70" />
+          <br></br>
+          PostgreSQL
+        </label>
+        <label>
+          <img src="https://cdn-icons-png.flaticon.com/512/873/873120.png" width="70" height="70" />
+          <br></br>
+          Heroku
+        </label>
+        <label>
+          <img src="https://cdn-icons-png.flaticon.com/512/906/906324.png" width="70" height="70" />
+          <br></br>
+          VS Code
+        </label>
+        <label>
+          <img src="https://cdn-icons-png.flaticon.com/512/2111/2111292.png" width="70" height="70" />
+          <br></br>
+          GitHub
+        </label>
+      </div>
+      <div ref={section2Ref}>Section 2 content</div>
+      <div ref={section3Ref}>Section 3 content</div>
+      <div ref={section4Ref}>Section 4 content</div>
     </div>
   );
 }
