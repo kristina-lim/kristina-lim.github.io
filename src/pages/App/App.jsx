0e0f409a0fd8308b4fc534dceba29a021e04912f
kristina-lim <kristinahlim@gmail.com>
2022-12-28 16:12:55 -0800

    add navbar and install bootstrap

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,23 +1,11 @@
+import NavBar from '../../components/NavBar/NavBar';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      
     </div>
   );
 }
-
-export default App;
