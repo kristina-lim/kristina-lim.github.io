79c4b49dc4b2a962007efdb53ca39c26ca2c5c69
kristina-lim <kristinahlim@gmail.com>
2023-04-13 14:10:33 -0700

    Add testimonial data array of objects

--- src/components/TestimonialData/TestimonialData.jsx
+++ src/components/TestimonialData/TestimonialData.jsx
@@ -2,8 +2,37 @@ import Image1 from "../../assets/testimonial1.png";
 import Image2 from "../../assets/testimonial2.png";
 import Image3 from "../../assets/testimonial3.png";
 
-export default function TestimonialData = [
+export const TestimonialData = [
   {
-    
+    id: 1,
+    image: Image1,
+    title: "Jim Clark",
+    description:
+      `Kris was very engaged and active throughout the 12-week cohort. 
+      She asked great questions and wanted to know as much as possible about a given technology. 
+      Kris always followed the instructions & guidance provided by her instructional team precisely and promptly. 
+      She demonstrated the ability to learn and absorb the curriculum well-above that of a typical student. 
+      Kris got along extremely well with her fellow classmates, was always willing to help others, 
+      and demonstrated leadership by organizing study sessions, etc. 
+      As Kristina's former Software Engineering Lead Instructor I can wholeheartedly recommend her for a role as a junior full-stack developer!`
+  },
+  {
+    id: 2,
+    image: Image2,
+    title: "Mario Recinos",
+    description:
+      `Kris was very immersed during her time in the General Assembly Software Engineer Immersive Cohort. 
+      Kris demonstrated excellent technical understanding of the material and even organized study sessions daily during after hours to help out fellow students. Kris always applied herself beyond what was expected and was always researching additional ways to add improved experiences in her projects. As Kris's former Instructional Associate I can attest that she would be an amazing addition to any team.`
+  },
+  {
+    id: 3,
+    image: Image3,
+    title: "Corey Summa",
+    description:
+    `It was a pleasure to work with Kristina on our project. 
+    She consistently provided valuable insights that helped guide us towards success. 
+    In addition to her programming skills, Kristina also has an exceptional eye for UI/UX design. 
+    She demonstrated patience, determination, and a strong commitment to delivering a polished and functional application. 
+    Kristina would be an asset to any development team.`
   },
 ]
