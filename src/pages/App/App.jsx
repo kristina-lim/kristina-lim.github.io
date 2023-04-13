8a44af514830e219e726a4fa459a1f9a8105227a
kristina-lim <kristinahlim@gmail.com>
2023-04-13 13:33:14 -0700

    Add Testimonials component in app

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,6 +4,7 @@ import Home from '../../components/Home/Home';
 import About from '../../components/About/About';
 import Skills from '../../components/Skills/Skills';
 import Project from '../../components/Project/Project';
+import Testimonials from '../../components/Testimonials/Testimonials';
 import Contact from '../../components/Contact/Contact';
 import Footer from '../../components/Footer/Footer';
 import ScrollUp from '../../components/ScrollUp/ScrollUp';
@@ -18,6 +19,7 @@ export default function App() {
         <About />
         <Skills />
         <Project />
+        <Testimonials />
         <Contact />
       </main>
       <Footer />
