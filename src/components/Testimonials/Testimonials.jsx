c25962db187a503c31c381c2bb8ac5f1bb44fc18
kristina-lim <kristinahlim@gmail.com>
2023-04-13 13:53:39 -0700

    Add title, subtitle, and data component

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Testimonials () {
   return (
-    <div>Testimonials</div>
+    <section className="testimonial container section">
+      <h2 className="section_title">Testimonials</h2>
+      <span className="section_subtitle">Recommendations from Instructors and Peers</span>
+      <div className="testimonials_container"></div>
+    </section>
   )
 }
