160c9d1ee77e32fcf6754fec5b4c452204c100e3
kristina-lim <kristinahlim@gmail.com>
2023-04-19 16:14:50 -0700

    Add css and add swiper

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,17 +1,42 @@
 import React from 'react'
 import { TestimonialData } from '../TestimonialData/TestimonialData';
-import './testimonials.css';
+import './Testimonials.css';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
-export default function Testimonials () {
+// Import required modules
+import { Pagination } from "swiper";
+
+export default function Testimonials() {
   return (
     <section className="testimonial container section">
       <h2 className="section_title">Testimonials</h2>
       <span className="section_subtitle">Recommendations from Instructors and Peers</span>
-      <div className="testimonial_container">
+      <Swiper className="testimonial_container"
+        loop={true}
+        grabCursor={true}
+        spaceBetween={24}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          576: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+          },
+        }}
+        modules={[Pagination]}
+      >
         {TestimonialData.map(({id, image, title, description}) => {
           return (
-            <div className="testimonial_card" key={id}>
+            <SwiperSlide className="testimonial_card" key={id}>
               <img 
                 src={image} 
                 alt=""
@@ -19,10 +44,10 @@ export default function Testimonials () {
               />
               <h3 className="testimonial_name">{title}</h3>
               <p className="testimonial_description">{description}</p>
-            </div>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </section>
-  )
-}
+  );
+};
